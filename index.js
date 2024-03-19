4f7207ba79916aa867bcfc06b96e0f1951f22382
Kyle <kylejrgurlad@gmail.com>
2024-03-19 10:34:30 +0300

    completed test

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(freeSample){
+  if (freeSample <= 400) {
+    return 'This one is on me!'
+  } else if ( freeSample > 400 && freeSample <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if ( freeSample > 2000 && freeSample <=2500 ) {
+    return 'I will gladly take your thirty bucks.'
+  } else if ( freeSample > 2500 ) {
+    return 'No can do.'
+  }
+
+}
+ 
+
+function ternaryCheckCity(theCity){
+ return theCity === "NYC" ? 'Ok, sounds good.': 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tipType){
+  let tipMe;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tipType) {
+    case 'generous' :
+     return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default : 
+    return 'Bye.'
+  
+  }
 }
